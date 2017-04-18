5b07440c217c11e59edad3895d890283d12ff46c
Lazy Code <fbenalex@gmail.com>
2017-04-18 16:16:37 +0300

    message form + input validation

--- src/components/Messages/MessageForm.jsx
+++ src/components/Messages/MessageForm.jsx
@@ -1,17 +1,40 @@
 import React, {Component} from 'react';
-import PropTypes from 'prop-types';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 
 class MessageForm extends Component {
+
+  state = {
+    value: '',
+    validationState: null
+  };
+
+  handleChange = (e) => {
+    const value = e.target.value.trim();
+    const validationState = (value.length < 2) ? 'error' : 'success';
+    this.setState({ value, validationState });
+  };
+
   render() {
     return (
-      <div>
-        Message Form
-      </div>
+      <form>
+        <FormGroup
+          controlId="formBasicText"
+          validationState={this.state.validationState}
+        >
+          <ControlLabel>Enter a message</ControlLabel>
+          <FormControl
+            type="text"
+            value={this.state.value}
+            placeholder="Enter text"
+            onChange={this.handleChange}
+          />
+          <FormControl.Feedback />
+        </FormGroup>
+      </form>
     );
   }
 }
-
-MessageForm.propTypes = {};
-MessageForm.defaultProps = {};
 
 export default MessageForm;
