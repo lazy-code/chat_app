2c286b5f8638e01d65f14bb6072a076e0b153322
Lazy Code <fbenalex@gmail.com>
2017-04-19 16:11:19 +0300

    messages output

--- src/components/Messages/Message.jsx
+++ src/components/Messages/Message.jsx
@@ -2,16 +2,36 @@ import React, {Component} from 'react';
 import PropTypes from 'prop-types';
 
 class Message extends Component {
+
+  // Format timestamp into date
+  formatDate = (timeStamp) => {
+    const dt = new Date(timeStamp);
+    let hours = dt.getHours();
+    let minutes = dt.getMinutes();
+    let seconds = dt.getSeconds();
+
+    hours = (hours < 10) ? '0' + hours : hours;
+    minutes = (minutes < 10) ? '0' + minutes : minutes;
+    seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+    return hours + ':' + minutes + ':' + seconds;
+  };
+
   render() {
+
+    const { message } = this.props;
+    const time = this.formatDate(message.timeStamp);
+
     return (
       <div>
-        Message
+        {time} - {message.text}
       </div>
     );
   }
 }
 
-Message.propTypes = {};
-Message.defaultProps = {};
+Message.propTypes = {
+  message: PropTypes.object.isRequired
+};
 
 export default Message;
--- src/components/Messages/MessageList.jsx
+++ src/components/Messages/MessageList.jsx
@@ -1,17 +1,29 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Message from './Message';
+import Well from 'react-bootstrap/lib/Well';
 
 class MessageList extends Component {
   render() {
+
+    const { messages } = this.props;
+    const body = messages.map((message) => {
+      return <Message message={message} key={message.timeStamp} />
+    });
+
     return (
       <div>
-        Message List
+        <Well bsSize="small">
+          <h3>Messages</h3>
+          {body}
+        </Well>
       </div>
     );
   }
 }
 
-MessageList.propTypes = {};
-MessageList.defaultProps = {};
+MessageList.propTypes = {
+  messages: PropTypes.array.isRequired
+};
 
 export default MessageList;
