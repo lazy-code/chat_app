45db2fd7969ff9c98bab8b8f2607227242958a9e
Lazy Code <fbenalex@gmail.com>
2017-04-19 13:34:43 +0300

    server actions

--- server.js
+++ server.js
@@ -10,6 +10,20 @@ const server = app.listen(3000);
 const io = require('socket.io').listen(server);
 
 io.sockets.on('connection', function (socket) {
+
+  socket.once('disconnect', function () {
+    connections.splice(connections.indexOf(socket), 1);
+    socket.disconnect();
+    console.log('Disconnected: %s sockets connected', connections.length);
+    io.emit('disconnect');
+  });
+
+  socket.on('messageAdded', function (payload) {
+    const { timeStamp, text } = payload;
+    const newMessage = { timeStamp, text };
+    io.emit('messageAdded', newMessage);
+  });
+
   connections.push(socket);
   console.log('Connected: %s sockets connected', connections.length);
 });
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,26 +10,49 @@ import './bootstrap.css';
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {};
-    this.socket = io('http://localhost:3000');
-    this.socket.on('connect', this.connect);
+
+    this.state = {
+      status: 'disconnected',
+      messages: [{
+        timeStamp: Date.now(),
+        text: 'Welcome to chat'
+      }],
+      users: [],
+      user: ''
+    };
   }
 
-  connect = () => {
-    this.setState({status: 'connected'});
-    console.log('Connected ', this.socket.id);
+  componentWillMount = () => {
+    this.socket = io('http://localhost:3000');
+    this.socket.on('connect', this.connect);
+    this.socket.on('messageAdded', this.messageAdded);
   };
 
+  // Add connection
+  connect = () => this.setState({ status: 'connected' });
+
+  // Remove connection
+  disconnect = () => this.setState({ status: 'disconnected' });
+
+  // Message was added
+  messageAdded = (message) => (
+    this.setState({ messages: this.state.messages.concat(message) })
+  );
+
+  // Emit new message
+  emit = (eventName, payload) => this.socket.emit(eventName, payload);
+
   render() {
+
     return (
       <Grid>
         <Row>
           <Col md={4}>
-            <UserList />
+            <UserList {...this.state} />
           </Col>
           <Col md={8}>
-            <MessageList />
-            <MessageForm />
+            <MessageList {...this.state} />
+            <MessageForm {...this.state} emit={this.emit}/>
           </Col>
         </Row>
       </Grid>
